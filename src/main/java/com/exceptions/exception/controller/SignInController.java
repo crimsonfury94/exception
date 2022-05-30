bcc054ab8bfd5d84a4c42aaf6fb7ea5e68d05327
Dmitry <leon1861@yandex.ru>
2022-05-30 22:47:18 +0700

    Пока что все!

--- src/main/java/com/exceptions/exception/controller/SignInController.java
+++ src/main/java/com/exceptions/exception/controller/SignInController.java
@@ -3,8 +3,8 @@ package com.exceptions.exception.controller;
 import com.exceptions.exception.exceptions.WrongLoginException;
 import com.exceptions.exception.exceptions.WrongPasswordException;
 import com.exceptions.exception.service.SignInService;
-import com.sun.source.tree.PatternTree;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,17 +12,27 @@ import java.util.regex.Pattern;
 
 
 @RestController
+@RequestMapping(path = "/sign_in")
 public class SignInController {
 
 
-    @GetMapping(path = "/sign_in")
-    public static boolean signIn(@RequestParam(value = "login") String login,
-                                 @RequestParam(value = "password") String password,
-                                 @RequestParam(value = "confirmPassword") String confirmPassword) {
+    private final SignInService signInService;
+
+    public SignInController(SignInService signInService) {
+        this.signInService = signInService;
+    }
+
+
+
+    @GetMapping(path = "/")
+    public static boolean signInAccount(String login,
+                                         String password,
+                                        String confirmPassword) {
+
         try {
-            if (login.length() <= 20 && password.length() < 20 && password.equals(confirmPassword)) {
+            if (login.length() <= 20 || password.length() < 20 || password.equals(confirmPassword)) {
                 return true;
-            } else if (Pattern.matches("[a-zA-Z0-9_]+", login) && Pattern.matches("[a-zA-Z0-9_]+", password) && Pattern.matches("[a-zA-Z0-9_]+", confirmPassword)) {
+            } else if (Pattern.matches("[a-zA-Z0-9_]+", login) || Pattern.matches("[a-zA-Z0-9_]+", password) || Pattern.matches("[a-zA-Z0-9_]+", confirmPassword)) {
                 return true;
             }
         } catch (WrongLoginException e) {
@@ -33,5 +43,14 @@ public class SignInController {
                 return false;
             }
         }
+        return true;
+    }
+
+    @GetMapping(path = "/welcome")
+    public String signIn(@RequestParam(value = "login") String login,
+                         @RequestParam(value = "password") String password,
+                         @RequestParam(value = "confirmPassword") String confirmPassword) {
+
+        return signInService.signIn(login, password, confirmPassword);
     }
 }
